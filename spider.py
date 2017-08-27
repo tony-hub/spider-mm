007850c95f4c2212f0e69d3a05972a65932aed3c
tony <1156391589@qq.com>
2017-08-27 20:04:14 +0800

    添加注释

--- spider.py
+++ spider.py
@@ -12,7 +12,7 @@ def spider():
     while x<307:
         url = 'http://www.mzitu.com/zipai/comment-page-%s/'%n
         headers = {
-           # 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+           # 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',  我以为他有反扒机制 ，其实并没有
 
         }
         url = urllib2.Request(url,headers=headers)
@@ -21,16 +21,16 @@ def spider():
         # req =r'data-original="(.*?)"'
         # req = re.compile(req)
         # response = req.findall(html)
-        response =BeautifulSoup(html,'html.parser').find_all('img',src=re.compile('sinaimg.cn'))
+        response =BeautifulSoup(html,'html.parser').find_all('img',src=re.compile('sinaimg.cn'))   #用Beautifulsoup 解析hmtl  找到src包含‘sinaimg.cn’的img标签
         # print(response)
         if not os.path.exists(r'D:\img'):
-            os.makedirs(r'D:\img')
+            os.makedirs(r'D:\img')   #判断D盘是否存在img文件夹，不存则这创建
         for i  in response:
             n +=1
-            img=i['src']
+            img=i['src']   #获取图片的链接地址
             print(img)
             print '正在保存第%s张到D盘的img文件夹'%n
-            urllib.urlretrieve(img,r'D:\img\%s.%s'%(n,img.split('.')[-1]))
+            urllib.urlretrieve(img,r'D:\img\%s.%s'%(n,img.split('.')[-1]))   #下载图片，并把图片命名并保留原来的后缀名
         x+=1
     print('爬取完毕')
 if '__main__'==__name__:
